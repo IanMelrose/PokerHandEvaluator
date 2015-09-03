8de03a00a9ec909627abda48362f3e9a76c9e1ce
IanMelrose <ianmelrose@hotmail.co.uk>
2015-09-03 20:55:42 +0100

    Adding the project files
    Main program class, config file and the project file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker_Hand_Evaluator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck theDeck = new Deck();
+            theDeck.Shuffle();
+        }
+    }
+}
