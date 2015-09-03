da0aa2e23d4ae01dce7eb53c3734bf19b08d294e
IanMelrose <ianmelrose@hotmail.co.uk>
2015-09-03 21:01:15 +0100

    Created Constructor, AddCard & GetCard Methods to Hand.cs

--- Hand.cs
+++ Hand.cs
@@ -8,5 +8,21 @@ namespace Poker_Hand_Evaluator
 {
     class Hand
     {
+        private List<Card> theHand;
+
+        public Hand()
+        {
+            theHand = new List<Card>();
+        }
+
+        public void AddCard(Card theCard)
+        {
+            theHand.Add(theCard);
+        }
+
+        public List<Card> GetCards()
+        {
+            return theHand;
+        }
     }
 }
